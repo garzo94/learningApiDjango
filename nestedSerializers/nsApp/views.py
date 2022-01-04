f32ac10040ce201e0d00238a163016181e95c2e2
garzo94 <alexgarzo25@gmail.com>
2022-01-04 10:29:08 -0600

    Security

--- nestedSerializers/nsApp/views.py
+++ nestedSerializers/nsApp/views.py
@@ -4,12 +4,19 @@ from .models import Author, Book
 from nsApp.serializer import AuthorSerializer, BookSerializer
 from rest_framework import generics
 
+#SECURITY (goes in in Settings for global security)
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+
 # Create your views here.
 
 
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    #Authentication
+    # authotentication_classes = [BasicAuthentication]
+    # permission_classes = [IsAuthenticated, DjangoModelPermissions] # display message of not pemission
 
 
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
